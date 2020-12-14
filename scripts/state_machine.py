ad9f901ad56aa7d0a3d9f339d650574901b39065
sara <sara.romano.15@gmail.com>
2020-12-14 16:24:18 +0100

    Final State Machine

--- scripts/state_machine.py
+++ scripts/state_machine.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 ## @file state_machine.py 
-# @brief This node implements a state machine 
+# @brief This node implements a state machine which permits to move around and to search for a ball, go to sleep, and play with the ball when the last one is found. 
 # 
-# Details: It is the main node: it receives commands from the commander node, implements the state machine and sends the target positions to display node. 
-# 
+
 
 #ros + python library 
 import rospy
@@ -13,7 +12,7 @@ import time
 import math
 import random
 
-#state machine 
+#smach library 
 import smach 
 import smach_ros
 
@@ -21,7 +20,7 @@ import smach_ros
 from geometry_msgs.msg import Point, Twist, Pose, PoseStamped 
 from sensor_msgs.msg import LaserScan 
 from sensor_msgs.msg import CompressedImage 
-from std_msgs.msg import String, Float64 #commands 
+from std_msgs.msg import String, Float64 
 from tf import transformations 
 from gazebo_msgs.msg import LinkState 
 from nav_msgs.msg import Odometry 
@@ -46,70 +45,130 @@ VERBOSE = False
 vel = Twist() 
 angle_camera = Float64() 
 
+
 global count
 global camera_rotate
+global count2
+global DetectedBall
 
 
-#random choice 
+## User Action function 
 def user_action(): 
-    return random.choice(['play', 'sleep'])
+    ## random choice between search for the ball or go to sleep 
+    return random.choice(['searchball', 'sleep'])
 
 
 
-#client of actionlib server for the dog robot which moves randomly in Normal state
-def move_dog_randomly(): 
-    #Creates the SimpleActionClient, passing the type of the action (PlanningAction) to the constructor
+## client of actionlib server for moving the dog robot 
+def move_dog(target): 
+
+    ## Creates the SimpleActionClient, passing the type of the action (PlanningAction) to the constructor
     client = actionlib.SimpleActionClient(
         '/robot_reaching_goal', exp_assignment2.msg.PlanningAction)
 
-     # Waits until the action serve has started up and started listening for goal 
+     ## Waits until the action serve has started up and started listening for goal 
     client.wait_for_server() 
 
-    #Create a goal to sent to action server 
+    ## Create a goal to sent to action server 
     goal = exp_assignment2.msg.PlanningGoal() 
-    goal.target_pose.pose.position.x = random.randrange(-8,8,1)  
-    goal.target_pose.pose.position.y = random.randrange(-8,8,1)
-    goal.target_pose.pose.position.z = 0
+    goal.target_pose.pose.position.x = target[0]
+    goal.target_pose.pose.position.y = target[1]
+    goal.target_pose.pose.position.z = target[2]
 
-    #Send the goal to the action server 
+    ## Send the goal to the action server 
     client.send_goal(goal) 
 
-    #waits for the serve to finish performing the action. 
+    ## waits for the serve to finish performing the action. 
     client.wait_for_result() 
 
     return client.get_result() 
 
 
-#client of actionlib serve for the dog robot which goes to sleep 
-def move_dog_home(): 
-    #Creates the SimpleActionClient, passing the type of the action (PlanningAction) to the constructor
-    client = actionlib.SimpleActionClient(
-        '/robot_reaching_goal', exp_assignment2.msg.PlanningAction)
-
-     # Waits until the action serve has started up and started listening for goal 
-    client.wait_for_server() 
-
-    #Create a goal to sent to action server 
-    home = exp_assignment2.msg.PlanningGoal() 
-    home.target_pose.pose.position.x = 7
-    home.target_pose.pose.position.y = 7
-    home.target_pose.pose.position.z = 0
-
-    #Send the goal to the action server 
-    client.send_goal(home) 
-
-    #waits for the serve to finish performing the action. 
-    client.wait_for_result() 
-
-    return client.get_result() 
 
 
+## Publisher 
+image_pub = rospy.Publisher("/output/image_raw/compressed",
+                                         CompressedImage, queue_size=1)
+
+vel_pub = rospy.Publisher("/robot/cmd_vel", Twist, queue_size=1)
+
+
+## Callback for Normal State for subscribe to a Camera1 : it checks if the ball is on the arena, if it is, a parameter (DetectedBall) becomes 1 and the robot goes to play, otherwise the parameter becomes 0 and the robot moves randomly. 
+def Normal_clbk(ros_data): 
+    
+    global count2
+    global DetectedBall
+    global SearchBallSub
+
+    while rospy.get_param('count2') == 360: 
+        time.sleep(1) 
+
+    
+    ## direct conversion to CV2 
+    np_arr = np.fromstring(ros_data.data, np.uint8)
+    image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # OpenCV >= 3.0:
+    ## define color 
+    greenLower = (50, 50, 20)
+    greenUpper = (70, 255, 255)
+    ## create a mask 
+    blurred = cv2.GaussianBlur(image_np, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    ## find color
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+
+
+    ## only proceed if at least one contour was found
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        ## only proceed if the radius meets a minimum size
+        if radius > 10:
+            
+
+            cv2.circle(image_np, (int(x), int(y)), int(radius),
+                        (0, 255, 255), 2)
+            cv2.circle(image_np, center, 5, (0, 0, 255), -1)
+
+            ## the ball is found, set parameter to 1
+            rospy.set_param('DetectedBall',1)  
+
+    ## the ball is not found 
+    else:
+
+        vel_ = Twist()
+        ## set an angular velocity to search around 
+        vel_.angular.z = 0.5 
+        vel_pub.publish(vel_) 
+        count2 = rospy.get_param('count2')
+        count2 = count2 + 1 
+        rospy.set_param('count2', count2) 
+          
+
+    ## show window 
+    cv2.imshow('window', image_np)
+    cv2.waitKey(2)
+         
+
+
+## Istance called in play state to track the ball 
 class image_feature:
-
+    ## initialization 
     def __init__(self):
         '''Initialize ros publisher, ros subscriber'''
-        #rospy.init_node('image_feature', anonymous=True)
-     # topic where we publish
+
+        ## publisher 
         self.image_pub = rospy.Publisher("/output/image_raw/compressed",
                                          CompressedImage, queue_size=1)
 
@@ -117,38 +176,43 @@ class image_feature:
                                        Twist, queue_size=1)
  
 
-        # subscribed Topic
+        ## subscriber
         self.subscriber = rospy.Subscriber("/robot/camera1/image_raw/compressed",
                                            CompressedImage, self.callback,  queue_size=1)
 
+    ## callback to subscribe to camera1 when the state is Play: when the ball is found, a parameter (camera_rotate) becomes 1 and the robot starts to rotate the head, otherwise, a parameter becomes 0 and the robot search around for a bit time. 
     def callback(self, ros_data): 
         global count 
         global camera_rotate
         count = rospy.get_param('count') 
-        #while count == 700: 
-        #    time.sleep(1)
+
+        while count == 360: 
+            time.sleep(1)
+
+
+        
 
         if VERBOSE:
             print ('received image of type: "%s"' % ros_data.format)
 
-        #### direct conversion to CV2 ####
+        ## direct conversion to CV2 
         np_arr = np.fromstring(ros_data.data, np.uint8)
-        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # OpenCV >= 3.0:
-
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  
+        ## define color 
         greenLower = (50, 50, 20)
         greenUpper = (70, 255, 255)
-
+        ## create a mask
         blurred = cv2.GaussianBlur(image_np, (11, 11), 0)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, greenLower, greenUpper)
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
-        #cv2.imshow('mask', mask)
+        ## find color
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         center = None
-        # only proceed if at least one contour was found
+        ##  only proceed if at least one contour was found
         if len(cnts) > 0:
             
             # find the largest contour in the mask, then use
@@ -172,12 +236,13 @@ class image_feature:
                            (0, 255, 255), 2)
                 cv2.circle(image_np, center, 5, (0, 0, 255), -1)
                 vel = Twist()
+                ## define a velocity to track the ball
                 vel.angular.z = 0.002*(center[0]-400)
                 vel.linear.x = -0.01*(radius-100) 
                 
                 self.vel_pub.publish(vel)
-
-                if vel.linear.x < 0.01: 
+                ## if the robot reaches the ball then it wanna rotate the head: thus the parameter "rotate_camera"  is setted to 1
+                if (vel.linear.x < 0.01) & (vel.angular.z < 0.01): 
                     vel.angular.z = 0
                     vel.linear.x = 0
                 
@@ -185,28 +250,35 @@ class image_feature:
                     rospy.set_param('camera_rotate', 1)
                     
 
-
+        ## if the ball is not found, searches around 
         else:
+
             vel = Twist()
             vel.angular.z = 0.5 
             self.vel_pub.publish(vel) 
             count = count + 1 
+        
             rospy.set_param('count', count) 
 
+            ## after a bit time in which the ball is not found, the robot go to normal state. 
+            if rospy.get_param('count') == 259: 
+                
+                vel.angular.z = 0.0 
+                self.vel_pub.publish(vel) 
+                self.subscriber.unregister() 
 
 
 
 
-        # update the points queue
-        # pts.appendleft(center)
+
+        ## show the window 
         cv2.imshow('window', image_np)
         cv2.waitKey(2)                
 
 
 
 
-
-        ## Normal state definition    
+## Normal state definition    
 class Normal(smach.State):
 
     ## inizialization
@@ -220,26 +292,48 @@ class Normal(smach.State):
         
     ## execution 
     def execute(self, userdata):
-        ## set parameter service state = 1 
         
         
-        ## Main Loop 
+        global SearchBallSub, count2, DetectedBall
+
+        rospy.set_param('count',0)
+        
         while True: 
-            #move randomly 
-            rospy.set_param('count',0) 
-            move_dog_randomly()
             
-            time.sleep(5) 
-            
+            time.sleep(10) 
+            ## read command (choice random between move random or go to sleep)
+            self.command = user_action() 
+            rospy.loginfo('command received is %s', self.command) 
 
-            #self.command = user_action() 
-            #rospy.loginfo('command received is %s', self.command) 
+            ## command is sleep
+            if self.command == "sleep": 
+                ## switch in sleep state
+                return 'outcome1'
 
-            #if self.command == "sleep" :
-            #    return 'outcome1'
+            ## command is searchball
+            if self.command == "searchball": 
 
-            #if self.command == "play" : 
-            return 'outcome2'
+                ## subscriber to camera to search the ball 
+                SearchBallSub = rospy.Subscriber("camera1/image_raw/compressed",
+                                                    CompressedImage, Normal_clbk,  queue_size=1)
+
+                        
+                while rospy.get_param('count2') < 360: 
+                    time.sleep(1) 
+                    ## if the ball is found, switch to play state
+                    if rospy.get_param('DetectedBall') == 1: 
+                        rospy.loginfo('The ball is here, Play!')
+                        rospy.set_param('DetectedBall', 0)
+                        rospy.set_param('count2',0)
+                        SearchBallSub.unregister() 
+                        return 'outcome2'
+                ## the ball is not found
+                SearchBallSub.unregister() 
+                rospy.set_param('count2',0)
+                rospy.loginfo('The ball is not here')
+                ## call the function "move_dog" to move random in the arena 
+                move_dog([random.randrange(-8,8,1), random.randrange(-8,8,1), 0]) 
+        
 
 
 ## Sleep State definition 
@@ -252,7 +346,8 @@ class Sleep(smach.State):
     
     ## Execution 
     def execute(self, userdata):
-        move_dog_home()
+        rospy.loginfo('Go to sleep')
+        move_dog([7,7,0])
 
         ## sleep for a while 
         time.sleep(14) 
@@ -266,6 +361,7 @@ class Play(smach.State):
     def __init__(self): 
         ## 1 outcome defined : Normal 
         smach.State.__init__(self, outcomes=['outcome2'])
+        ## publisher for move the head 
         self.angle_pub = rospy.Publisher("/robot/joint_position_controller/command",
                                     Float64, queue_size=1) 
 
@@ -275,35 +371,35 @@ class Play(smach.State):
     def execute(self, userdata): 
 
 
-
+        ## called the instance to track the ball 
         ic = image_feature() 
 
         
 
-        while rospy.get_param('count') < 700: 
+        while rospy.get_param('count') < 259: 
 
             time.sleep(1)
+            ## look for the "camera_rotate" parameter 
             replay = rospy.get_param('camera_rotate')
+
             if replay == 1: 
+                rospy.loginfo('rotating head')
                 angle_camera = Float64() 
                 angle_camera.data = 0.0 
                     
-                        # rotate head
-                while angle_camera.data < 1.20: #head turns left
-                    #if rospy.get_param('camera_rotate') == 1: 
+                ## rotate head
+                while angle_camera.data < 0.5: #head turns left
                     angle_camera.data = angle_camera.data + 0.1 
                     self.angle_pub.publish(angle_camera) 
                     time.sleep(1)
                 time.sleep(3) 
                 
                 while angle_camera.data > 0.01: #head turns right 
-                    #if rospy.get_param('camera_rotate') == 1: 
                     angle_camera.data = angle_camera.data - 0.1 
                     self.angle_pub.publish(angle_camera) 
                     time.sleep(1)
 
-                while angle_camera.data > -1.20: 
-                    #if rospy.get_param('camera_rotate') == 1: 
+                while angle_camera.data > -0.5: 
                     angle_camera.data = angle_camera.data - 0.1 
                     self.angle_pub.publish(angle_camera)
                     time.sleep(1) 
@@ -317,21 +413,9 @@ class Play(smach.State):
 
 
                 rospy.set_param('camera_rotate', 0)
-                time.sleep(100)
+                time.sleep(30)
 
-                
-
-                        
-                
-
-                
-
-
-        #while rospy.get_param('camera_rotate') == 0: 
-        #    ic2 = image_feature() 
-        #    rospy.set_param('camera_rotate',1)
- 
-
+       
         return 'outcome2'
 
 
@@ -353,9 +437,10 @@ class Play(smach.State):
 def main():
     ## init the ros node 
     rospy.init_node('state_machine')
-  
+    rospy.set_param('count2',0)
     rospy.set_param('count',0) 
     rospy.set_param('camera_rotate',0)
+    rospy.set_param('DetectedBall',0) 
 
     ## Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['outcome4'])
